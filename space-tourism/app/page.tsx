25b135c925b801850bf319fddc142047f694121b
John J Eze <johneze.webdev03@gmail.com>
2025-06-04 12:15:45 +0000

    Space Data Added

--- space-tourism/app/page.tsx
+++ space-tourism/app/page.tsx
@@ -1,7 +1,12 @@
-import Image from "next/image";
+// import Image from "next/image";
+// import Link from "next/link";
+// import Router from "next/router";
 
 export default function Home() {
   return (
+    <div className="flex flex-col items-center justify-center gap-4  bg-gray-100">
     <h1 className="text-4xl font-bold text-center mt-10"> Home Page </h1>
+    
+    </div>
   );
 }
