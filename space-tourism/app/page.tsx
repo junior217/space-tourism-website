caafe0babf27711bad24dea199ea64ce4814ff17
John J Eze <johneze.webdev03@gmail.com>
2025-06-07 09:29:40 +0000

    More of main page done

--- space-tourism/app/page.tsx
+++ space-tourism/app/page.tsx
@@ -4,13 +4,13 @@
 
 export default function Home() {
   return (
-    <main className="p-10 flex flex-row items-center justify-center gap-10">
-    <div className="flex flex-col gap-5">
+    <main className="my-40 px-20 grid grid-cols-[1fr_auto] gap-10">
+    <div className="w-150 flex flex-col gap-5">
       <h2 className="text-3xl font-bold text-slate-50 font-serif">So, you want to travel to</h2>
-      <h1 className="text-lg text-slate-50 font-serif">Space</h1>
+      <h1 className="text-6xl text-slate-50 font-serif">Space</h1>
       <p className="text-base text-slate-50 font-serif ">Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
     </div>
-    <div className="w-[100px] h-[100px] bg-slate-50 rounded-full flex justify-center items-center ">
+    <div className="w-80 h-80 bg-slate-50 rounded-full flex justify-center items-center ">
       <h1>Explore</h1>
     </div>
     
