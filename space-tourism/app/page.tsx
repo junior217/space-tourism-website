7b7711c0f4828c5f878aeb444294812fe4b0bdb5
John J Eze <johneze.webdev03@gmail.com>
2025-06-07 09:33:02 +0000

    more of space tour main page done, almost complete

--- space-tourism/app/page.tsx
+++ space-tourism/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       <p className="text-base text-slate-50 font-serif ">Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
     </div>
     <div className="w-80 h-80 bg-slate-50 rounded-full flex justify-center items-center ">
-      <h1>Explore</h1>
+      <h1 className="text-4xl font-bold text-slate-900">Explore</h1>
     </div>
     
     </main>
