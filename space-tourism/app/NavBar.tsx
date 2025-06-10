b80a042b3adb1a7e4875ae4c56fea6387d1ccec2
John J Eze <johneze.webdev03@gmail.com>
2025-06-10 12:46:43 +0000

    Home Page Tablet Responsive Done

--- space-tourism/app/NavBar.tsx
+++ space-tourism/app/NavBar.tsx
@@ -2,11 +2,11 @@ import Link from 'next/link';
 import { CircleStarSVG, StraightLineSVG } from './components/SvgImg';
 export default function NavBar() {
   return (
-    <nav className="relative mt-5 w-fill flex items-center justify-between py-8 text-white">
+    <nav className="relative mt-5 w-fill flex items-center justify-between py-8 text-white sm:grid md:grid-cols-[1fr_auto] sm:gap-4">
       <div className='ml-15 w-[50px] h-[50px] flex items-center justify-center'>
       <CircleStarSVG/>
       </div>
-      <div className='absolute left-[300px] flex items-center justify-center lg:hidden'>
+      <div className='absolute left-[300px] flex items-center justify-center'>
       <StraightLineSVG />
       </div>
       <div className="w-2xl flex space-x-4 justify-center items-center py-6 bg-gradient-to-r bg-slate-300 backdrop-blur-md opacity-90 text-slate-100">
--- space-tourism/app/page.tsx
+++ space-tourism/app/page.tsx
@@ -4,18 +4,18 @@ import NavBar from "./NavBar";
 export default function Home() {
   return (
   <div className="wrapper">
-    <div className="py-4">
+    <div className="xl:py-4 md:py-4">
     <NavBar />
     </div>
     <main
-    className="mt-20 pt-4 px-20 grid grid-cols-[1fr_auto] gap-10 md:flex md:flex-col md:items-center md:justify-center">
-    <div className="w-150 flex flex-col gap-5">
-      <h2 className="text-3xl text-slate-50 md:text-center md:text-5xl">So, you want to travel to</h2>
-      <h1 className=" text-6xl text-slate-50 uppercase md:text-center md:text-8xl">Space...</h1>
-      <p className=" text-xl text-slate-50 md:text-center md:text-2xl">Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
+    className="xl:mt-20  xl:py-4 xl:px-20 xl:grid xl:grid-cols-[1fr_auto] xl:gap-10 md:flex md:flex-col md:items-center md:justify-center md:px-25  md:mt-20 md:gap-5">
+    <div className="xl:w-150 xl:flex xl:flex-col xl:gap-5 md:flex md:flex-col md:text-center md:gap-5">
+      <h2 className="xl:text-3xl xl:text-slate-50 xl:text-left md:text-center md:text-5xl md:text-slate-50">So, you want to travel to</h2>
+      <h1 className=" xl:text-6xl xl:text-slate-50 xl:text-left uppercase md:text-center md:text-8xl md:text-slate-50">Space...</h1>
+      <p className=" xl:text-xl text-slate-50 xl:text-left md:text-center md:text-2xl md:text-slate-50">Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
     </div>
-    <div className="w-80 h-80 bg-slate-50 rounded-full flex justify-center items-center md:w-90 md:h-90">
-      <h1 className="text-4xl uppercase">Explore</h1>
+    <div className="xl:w-80 xl:h-80 xl:bg-slate-50 rounded-full xl:flex xl:justify-center xl:items-center md:w-80 md:h-80 md:bg-slate-50 md:flex md:justify-center md:items-center md:mt-10">
+      <h1 className="xl:text-4xl uppercase md:text-3xl">Explore</h1>
     </div>
     </main>
    </div>
